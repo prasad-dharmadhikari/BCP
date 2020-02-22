fdbf3b04d152546c4d742978d50c40c475763494
prasad-dharmadhikari <59562096+prasad-dharmadhikari@users.noreply.github.com>
2020-02-22 18:21:21 +0530

    dryrty
    gfhrtfyhrt

--- DayOfWeek.sh
+++ DayOfWeek.sh
@@ -8,7 +8,7 @@ y0=$(( year-(14-month)/12 ))
 x=$(( y0+y0/4-y0/100+y0/400 ))
 m0=$(( month+12*((14-month)+12)-2 ))
 d0=$(( (day+x+31*m0/12)%7 ))
-
+#if statement
 if (( $d0 == 0 ))
 then
 	echo "Sunday"
